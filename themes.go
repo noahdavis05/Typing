d7424b495218ecf84b83ff5b949abe4ec90015c5
Noah Davis <146333718+noahdavis05@users.noreply.github.com>
2025-08-11 21:43:56 +0000

    Updated colour theme

--- themes.go
+++ themes.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"github.com/charmbracelet/lipgloss"
@@ -6,14 +6,14 @@ import (
 
 type colourTheme struct {
 	borderStyleDefault lipgloss.Style
-	borderStyleActive lipgloss.Style
-	tabTextDefault lipgloss.Style
-	tabTextActive lipgloss.Style
-	typeTextIncorrect lipgloss.Style
-	typeTextCorrect lipgloss.Style
-	typeTextDefault lipgloss.Style
-	normalText lipgloss.Style
-	countDownBar lipgloss.Style
+	borderStyleActive  lipgloss.Style
+	tabTextDefault     lipgloss.Style
+	tabTextActive      lipgloss.Style
+	typeTextIncorrect  lipgloss.Style
+	typeTextCorrect    lipgloss.Style
+	typeTextDefault    lipgloss.Style
+	normalText         lipgloss.Style
+	countDownBar       lipgloss.Style
 }
 
 func NewColourTheme(profile int) (colourTheme, error) {
@@ -26,43 +26,43 @@ func NewColourTheme(profile int) (colourTheme, error) {
 	return ct, nil
 }
 
-
 func (ct *colourTheme) loadDefaultColourTheme() {
+
 	ct.borderStyleActive = lipgloss.NewStyle().
 		Border(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color("62")).
+		BorderForeground(lipgloss.Color("#7F5AF0")).
 		Padding(1, 2)
 
 	ct.borderStyleDefault = lipgloss.NewStyle().
 		Border(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color("#B0C4DE")). 
+		BorderForeground(lipgloss.Color("#94A1B2")).
 		Padding(1, 2)
 
 	ct.tabTextDefault = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#5F9EA0")).
-		Padding(0, 1)
+		Foreground(lipgloss.Color("#72757E")).
+		Padding(0, 2)
 
 	ct.tabTextActive = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#4682B4")). 
+		Foreground(lipgloss.Color("#7F5AF0")).
 		Bold(true).
 		Underline(true).
-		Padding(0, 1)
+		Padding(0, 2)
 
 	ct.typeTextIncorrect = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#FF6F61")). 
+		Foreground(lipgloss.Color("#FF6F61")).
 		Bold(true).
 		Underline(true)
 
 	ct.typeTextCorrect = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#65F527")). 
+		Foreground(lipgloss.Color("#2CB67D")).
 		Bold(true)
 
 	ct.typeTextDefault = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#DAE5EB")) 
+		Foreground(lipgloss.Color("#94A1B2"))
 
 	ct.normalText = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#DAE5EB")) 
+		Foreground(lipgloss.Color("#94A1B2"))
 
 	ct.countDownBar = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#4682B4")) 
+		Foreground(lipgloss.Color("#7F5AF0"))
 }
